33a89ceacd7fa88ce3401f3ba23ca6de8a8c35ec
emanuel <emanuel2005batista@gmail.com>
2022-08-07 08:49:12 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class="container">
+        <form action="#" method="post">
+            <label for="nome">Nome</label>
+            <input type="text" name="nome" id="nome">
+            <label for="endereco">Endereço</label>
+            <input type="text" name="endereco" id="endereco">
+            <label for="telefone">Telefone</label>
+            <input type="text" name="telefone" id="telefone">
+            <input type="submit" value="enviar" id="enviar">
+        </form>
+        <table id="tabela">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Endereço</th>
+                    <th>Telefone</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tr>
+                <td>teste</td>
+                <td>teste</td>
+                <td>teste</td>
+                <td>teste</td>
+                <td>teste</td>
+            </tr>
+            <tr>
+                <td>teste</td>
+                <td>teste</td>
+                <td>teste</td>
+                <td>teste</td>
+                <td>teste</td>
+            </tr>
+            <tr>
+                <td>teste</td>
+                <td>teste</td>
+                <td>teste</td>
+                <td>teste</td>
+                <td>teste</td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
