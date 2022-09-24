7fe6568f2cc34b1e5ec22f8ec7e355356e3f747d
emanuel <emanuel2005batista@gmail.com>
2022-09-24 19:50:53 -0300

    reformulação de todo o projeto

---
+++ App.php
@@ -0,0 +1,72 @@
+<?php
+class App {
+    private $servername;
+    private $username;
+    private $password;
+    private $database;
+    public $table;
+
+    public function __construct($servername, $username, $password, $database){
+        $this->servername = $servername;
+        $this->username = $username;
+        $this->password = $password;
+        $this->database = $database;
+        $this->table = [];
+    }
+
+    public function showData($table){
+        foreach($table as $row){
+            echo "<tr>";
+            foreach($row as $i){
+                echo "<td>".$i."</td>";
+            }
+            echo "<td><button class='del'>deletar</button><button class='edit'>editar</button>";
+            echo "</tr>";
+        }
+    }
+
+    public function connect(){
+        try {
+            $conn = new PDO("mysql:host=$this->servername;dbname=$this->database", $this->username, $this->password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          } catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+          }
+        return $conn;
+    }
+
+    public function insert($conn){
+        $dataUser = [
+            $_GET['nome'],
+            $_GET['nascimento'],
+            $_GET['renda']
+        ];
+        try{
+            $query = "INSERT INTO aluno(nome, nascimento, renda) VALUES ('$dataUser[0]', '$dataUser[1]', '$dataUser[2]')";
+            $conn->exec($query);
+        } catch(PDOException $e){
+            echo $query . "<br>" . $e->getMessage();
+        }
+        $conn = null;
+    }
+
+    public function select($conn){
+        $data = $conn->query("SELECT * FROM `aluno`")->fetchAll();
+        foreach($data as $row){
+            $arr = [
+                $row['id'],
+                $row['nome'],
+                $row['nascimento'],
+                "R$".$row['renda']
+            ];
+            array_push($this->table, $arr);
+        }
+        $conn = null;
+        $this->showData($this->table);
+    }
+
+    public function delete($conn, $id){}
+
+    public function update($conn, $id){}
+}
+?>
--- index.php
+++ index.php
@@ -1,9 +1,16 @@
 <?php
-    require "assets/backend/connect.php";
-    if(isset($_GET['err']) and $_GET['err'] == '1'){
-        $err = true;
-    } else {
-        $err = false;
+    require "App.php";
+    $app = new App("localhost", "root", "massa123", "pw2");
+
+    if(isset($_GET['nome']) && isset($_GET['nascimento']) && isset($_GET['renda'])){
+        $app->insert($app->connect());
+        header("location: index.php");
+    }
+
+    if(isset($_GET['del'])){
+        
+    } else if(isset($_GET['edit'])){
+        
     }
 ?>
 <!DOCTYPE html>
@@ -12,61 +19,60 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Document</title>
+    <style>
+        body {
+            min-height: 100vh;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+
+        form {
+            border: 1px solid black;
+            padding: 1rem;
+            margin-bottom: 1rem;
+        }
+
+        table, tr, td, th {
+            border: 1px solid #000;
+            padding: 1rem;
+        }
+    </style>
 </head>
 <body>
-    <div class="container">
-        <form action="assets/backend/adicionar.php" method="post">
-            <label for="nome">Nome</label>
-            <input type="text" name="nome" id="nome" placeholder="fulano" required>
-            <label for="endereco">Endereço</label>
-            <input type="text" name="endereco" id="endereco" placeholder='rua fodaRuaMuitoFoda 6669' required>
-            <label for="telefone">Telefone</label>
-            <input type="tel" pattern="[0-9]{2}-[0-9]{4}-[0-9]{4}" name="telefone" id="telefone" required placeholder="00-0000-0000">
-            <input type="submit" value="enviar" id="enviar">
-        </form>
-        <?php
-            if($err){
-                echo "<div id='erro'>já existe alguém com o mesmo endereço e telefone</div>";
-            }
-        ?>
-        <table id="tabela">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Endereço</th>
-                    <th>Telefone</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <?php
-                $resultado = $conn->query("SELECT * FROM `dados`");
-                if($resultado->num_rows > 0){
-                    while($row = $resultado->fetch_assoc()){
-                        echo "
-                        <tr>
-                            <td>".$row['id']."</td>
-                            <td>".$row['nome']."</td>
-                            <td>".$row['endereco']."</td>
-                            <td>".$row['telefone']."</td>
-                            <td><button class='tabelaBtn editar'>Editar</button><button class='tabelaBtn excluir'>Excluir</button></td>
-                        </tr>";
-                    }
-                }
-            ?>
-        </table>
-    </div>
+    <form action="#" method="get">
+        <label for="nome">nome</label>
+        <input type="text" name="nome" id="nome" required><br>
+        <label for="nascimento">nascimento</label>
+        <input type="date" name="nascimento" id="nascimento" required><br>
+        <label for="renda">renda</label>
+        <input type="number" name="renda" id="renda" required><br>
+        <input type="submit" value="enviar">
+    </form>
+    <table>
+        <tr>
+            <th>id</th>
+            <th>nome</th>
+            <th>nascimento</th>
+            <th>renda</th>
+        </tr>
+    <?php
+        $app->select($app->connect());
+    ?>
+    </table>
     <script>
-        document.querySelectorAll(".tabelaBtn").forEach(i => {
-            i.addEventListener("click", () => {
-                let id = i.parentNode.parentNode.children[0].textContent;
-                if(i.textContent == 'Excluir'){
-                    window.location.href='assets/backend/remover.php?id='+id;
-                } else {
-                    window.location.href='assets/backend/editar.php?id='+id;
-                }
+        let del = document.querySelectorAll(".del");
+        let edit = document.querySelectorAll(".edit");
+        del.forEach(e => {
+            e.addEventListener("click", () => {
+                window.location.href = "index.php?del="+e.parentElement.parentElement.children[0].textContent;
+            })
+        })
+        edit.forEach(e => {
+            e.addEventListener("click", () => {
+                window.location.href = "index,php?edit="+e.parentElement.parentElement.children[0].textContent;
             })
         })
     </script>
